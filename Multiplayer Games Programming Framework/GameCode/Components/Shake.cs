8997467b1dceadad8a8ac1a02e143e79982f6563
Jonathan-L-Davidson <d016527j@student.staffs.ac.uk>
2023-10-16 16:18:20 +0100

    Initial network

---
+++ Multiplayer Games Programming Framework/GameCode/Components/Shake.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplayer_Games_Programming_Framework.GameCode.Components
+{
+    internal class Shake : Component
+    {
+        float m_Speed;
+        SpriteRenderer m_spriteRenderer;
+        public Shake(GameObject gameObject) : base(gameObject)
+        {
+        }
+
+        protected override void Start(float deltaTime)
+        {
+            m_spriteRenderer = m_GameObject.GetComponent<SpriteRenderer>();
+            if (m_spriteRenderer == null)
+            {
+                throw new Exception("Object does not contain a sprite renderer.");
+            }
+        }
+        public void Init(float speed)
+        {
+            m_Speed = speed;
+        }
+
+        //public void Shake(Vector2 shakeBounds, int duration)
+        //{
+        //    Random rand = new Random();
+        //    int timer = 0;
+        //    while (timer < duration)
+        //    {
+        //        float randX = (float)rand.Next(shakeBounds.X, shakeBounds.Y);
+        //        float randY = (float)rand.Next(shakeBounds.X, shakeBounds.Y);
+                
+        //        timer++;
+
+        //    }
+        //}
+
+    }
+}
--- Multiplayer Games Programming Server/Server.cs
+++ Multiplayer Games Programming Server/Server.cs
@@ -2,29 +2,65 @@
 using System.Net.Sockets;
 using System.Net;
 using Multiplayer_Games_Programming_Packet_Library;
+using System.Text;
 
 namespace Multiplayer_Games_Programming_Server
 {
-	internal class Server
-	{
-		TcpListener m_TcpListener;
+    internal class Server
+    {
+        TcpListener m_TcpListener;
 
-		ConcurrentDictionary<int, ConnectedClient> m_Clients;
+        ConcurrentDictionary<int, ConnectedClient> m_Clients;
 
-		public Server(string ipAddress, int port)
-		{
-		}
+        public Server(string ipAddress, int port)
+        {
+            IPAddress ip = IPAddress.Parse(ipAddress);
+            m_TcpListener = new TcpListener(ip, port);
 
-		public void Start()
-		{
-		}
 
-		public void Stop()
-		{
-		}
+        }
 
-		private void ClientMethod(int index)
-		{
-		}
-	}
+        public void Start()
+        {
+            try
+            {
+                m_TcpListener.Start();
+                Console.WriteLine($"Server started on {m_TcpListener.LocalEndpoint}");
+
+                Socket socket = m_TcpListener.AcceptSocket();
+                Console.WriteLine("Connection made");
+
+                ClientMethod(socket);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Stop()
+        {
+            m_TcpListener?.Stop();
+        }
+
+        private void ClientMethod(Socket index)
+        {
+            try
+            {
+                string message;
+                NetworkStream stream = new NetworkStream(index, false);
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+    }
 }
